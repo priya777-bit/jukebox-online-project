dba68058e346bb74e6980644d055d56a0c473fba
twinkledahale98@gmail.com <twinkledahale98@gmail.com>
2021-12-06 16:51:57 +0530

    er

--- JUKEBOX-OFF-Project/src/DbOperation.java
+++ JUKEBOX-OFF-Project/src/DbOperation.java
@@ -4,6 +4,8 @@ import static java.sql.Statement.*;
 
 public class DbOperation
 {
+    Song song = new Song();
+
     public int getArtistId(String artist_name,String artist_gender)
     {
         int artist_id=0;
@@ -183,11 +185,13 @@ public class DbOperation
     }
     public boolean addSong(String song_name,String song_duration,int artist_id,int album_id,int genre_id)
     {
-        int artist_id = getArtistId(artist_name,artist_gender);
+        boolean result = false;
 
-        int album_id = getAlbumId(album_name,album_release_date);
+         artist_id = getArtistId(song.getArtist_name(),song.getArtist_gender());
 
-        int genre_id = getGenreId(genre_name);
+         album_id = getAlbumId(song.getAlbum_name(),new java.sql.Date(song.getAlbum_release_date().getTime()));
+
+         genre_id = getGenreId(song.getGenre_name());
 
         try
         {
@@ -196,7 +200,25 @@ public class DbOperation
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jukebox","root","root");
             System.out.println("Connection Success..");
 
-            String query = "insert into song (song_name,)"
+            String query = "insert into song (song_name,song_duration,artist_id,album_id,genre_id) values(?,?,?,?,?)";
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setString(1,song_name);
+            pst.setString(2,song_duration);
+            pst.setInt(3,artist_id);
+            pst.setInt(4,album_id);
+            pst.setInt(5,genre_id);
+
+            int result2 = pst.executeUpdate();
+
+            if(result2==1)
+            {
+                result = true;
+            }
         }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+        return result;
     }
 }
