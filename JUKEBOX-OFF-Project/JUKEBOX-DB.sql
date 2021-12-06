c559f49cfdfadb00b90185dc19f75cd51b01ba63
twinkledahale98@gmail.com <twinkledahale98@gmail.com>
2021-12-06 14:31:33 +0530

    er

---
+++ JUKEBOX-OFF-Project/JUKEBOX-DB.sql
@@ -0,0 +1,61 @@
+-- Creating Database Of Juke-Box
+
+create database jukebox;
+use jukebox;
+
+-- Creating Table Genre ..
+
+create table genre
+(
+	genre_id int primary key auto_increment,
+    genre_name char(50)
+);
+
+drop table genre;
+
+-- Creating Table Album ..
+
+create table album
+(
+	album_id int primary key auto_increment,
+    album_name char(50),
+    album_release_date date
+
+);
+
+drop table album;
+
+-- Creating Artist Table ..
+
+create table artist
+(
+	artist_id int primary key auto_increment,
+    artist_name char(50),
+    artist_gender char(20)
+);
+
+drop table artist;
+
+-- Creating Table Song ..
+
+create table song
+(
+	song_id int primary key auto_increment,
+    song_name char(100),
+    song_duration char(20),
+    genre_id int,
+    album_id int,
+    artist_id int,
+    foreign key(genre_id) references genre(genre_id),
+    foreign key(album_id) references album(album_id),
+    foreign key(artist_id) references artist(artist_id)
+);
+
+-- Creating View For All Tables ..
+
+create view gen_alb_art_song
+select g.genre_name,al.album_name,al.album_release_date
+,at.artist_name,at.artist_gender,s.song_name,s.song_duration from genre 
+, album  , artist , song as g , al , at , s;
+
+
---
+++ JUKEBOX-OFF-Project/src/JukeBox.java
@@ -0,0 +1,23 @@
+import java.util.Date;
+
+public class JukeBox
+{
+    private int genre_id;
+    private String genre_name;
+
+    private int album_id;
+    private String album_name;
+    private Date album_release_date;
+
+    private int artist_id;
+    private String artist_name;
+    private String artist_gender;
+
+    private int song_id;
+    private String song_name;
+    private String song_duration;
+
+    //Getter And Setter
+    
+
+}
