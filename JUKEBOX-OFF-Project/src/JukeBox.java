5e2ad1775e275aa07157aa0130e3967847db7ed1
twinkledahale98@gmail.com <twinkledahale98@gmail.com>
2021-12-06 15:00:05 +0530

    er

--- JUKEBOX-OFF-Project/src/JukeBox.java
+++ JUKEBOX-OFF-Project/src/JukeBox.java
@@ -2,44 +2,24 @@ import java.util.Date;
 
 public class JukeBox
 {
-    private int genre_id;
     private String genre_name;
 
-    private int album_id;
     private String album_name;
     private Date album_release_date;
 
-    private int artist_id;
     private String artist_name;
     private String artist_gender;
 
-    private int song_id;
     private String song_name;
     private String song_duration;
 
     //Getter And Setter
-    public int getGenre_id() {
-        return genre_id;
-    }
-
-    public void setGenre_id(int genre_id) {
-        this.genre_id = genre_id;
-    }
-
     public String getGenre_name() {
         return genre_name;
     }
 
     public void setGenre_name(String genre_name) {
         this.genre_name = genre_name;
-    }
-
-    public int getAlbum_id() {
-        return album_id;
-    }
-
-    public void setAlbum_id(int album_id) {
-        this.album_id = album_id;
     }
 
     public String getAlbum_name() {
@@ -58,14 +38,6 @@ public class JukeBox
         this.album_release_date = album_release_date;
     }
 
-    public int getArtist_id() {
-        return artist_id;
-    }
-
-    public void setArtist_id(int artist_id) {
-        this.artist_id = artist_id;
-    }
-
     public String getArtist_name() {
         return artist_name;
     }
@@ -80,14 +52,6 @@ public class JukeBox
 
     public void setArtist_gender(String artist_gender) {
         this.artist_gender = artist_gender;
-    }
-
-    public int getSong_id() {
-        return song_id;
-    }
-
-    public void setSong_id(int song_id) {
-        this.song_id = song_id;
     }
 
     public String getSong_name() {
